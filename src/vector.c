13262d8e6edbe4f88b7209b2aa0f0cd1926311ca
Connor Fletcher <connorfletcher72@gmail.com>
2026-08-06 19:36:42 -0700

    adding a more robust vector library

--- src/vector.c
+++ src/vector.c
@@ -14,11 +14,11 @@ Brief: Functions performing fundamental vector operations
 
 Vector *vector_create(unsigned int size) {
     if (size == 0) return NULL;
-    Vector *new_vector = calloc(1, sizeof(*new_vector));
+    Vector *new_vector = calloc(1, sizeof(new_vector->data));
 
     new_vector->size = size;
 
-    new_vector->data = calloc(size, sizeof(new_vector->data));
+    new_vector->data = calloc(size, sizeof(scalar_t));
     if (new_vector->data == NULL) {
         free(new_vector);
         return NULL;
@@ -32,4 +32,88 @@ void vector_destroy(Vector *vector) {
 
     free(vector->data);
     free(vector);
+}
+
+Vector *vector_add(const Vector *first_vector, const Vector *second_vector) {
+    if (first_vector->size != second_vector->size) return NULL;
+
+    Vector *sum = vector_create(first_vector->size);
+
+    if (sum == NULL) return NULL;
+
+    for (int i = 0; i < sum->size; i++) {
+        sum->data[i] = first_vector->data[i] + second_vector->data[i];
+    }
+
+    return sum;
+}
+
+Vector *vector_subtract(const Vector *first_vector, const Vector *second_vector) {
+    if (first_vector->size != second_vector->size) return NULL;
+
+    Vector *difference = vector_create(first_vector->size);
+
+    if (difference == NULL) return NULL;
+
+    for (int i = 0; i < difference->size; i++) {
+        difference->data[i] = first_vector->data[i] - second_vector->data[i];
+    }
+
+    return difference;
+}
+
+scalar_t vector_dot(const Vector *first_vector, const Vector *second_vector) {
+    if (first_vector->size != second_vector->size) return NUMBER_ERROR;
+    scalar_t dot_product = 0;
+    for (int i = 0; i < first_vector->size; i++) {
+        dot_product += first_vector->data[i] * second_vector->data[i];
+    }
+    return dot_product;
+}
+
+Vector *vector_cross(const Vector *lhs, const Vector *rhs) {
+    if (lhs->size != 3) return NULL;
+    if (rhs->size != 3) return NULL;
+
+    Vector *cross_product = vector_create(VECTOR_3D);
+
+    cross_product->data[0] =
+        lhs->data[1] * rhs->data[2] -
+        lhs->data[2] * rhs->data[1];
+
+    cross_product->data[1] =
+        lhs->data[2] * rhs->data[0] -
+        lhs->data[0] * rhs->data[2];
+
+    cross_product->data[2] =
+        lhs->data[0] * rhs->data[1] -
+        lhs->data[1] * rhs->data[0];
+
+    return cross_product;
+}
+
+scalar_t mag(const Vector *vector) {
+    scalar_t mag = 0;
+
+    for (int i = 0; i < vector->size; i++) {
+        mag += vector->data[i] * vector->data[i];
+    }
+
+    return sqrt(mag);
+}
+
+Vector *norm(const Vector *vector) {
+    if (vector == NULL) return NULL;
+
+    scalar_t magnitude = mag(vector);
+    if (magnitude == 0) return NULL;
+
+    Vector *norm = vector_create(vector->size);
+    if (norm == NULL) return NULL;
+
+    for (int i = 0; i < norm->size; i++) {
+        norm->data[i] = vector->data[i] / magnitude;
+    }
+
+    return norm;
 }
--- src/vector.h
+++ src/vector.h
@@ -1,6 +1,8 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 #include "vec3.h"
+#define NUMBER_ERROR -1
+#define VECTOR_3D 3
 
 typedef struct {
     unsigned int size;
@@ -22,5 +24,63 @@ Vector *vector_create(unsigned int size);
  * @param *vector Vector to be destroyed
  */
 void vector_destroy(Vector *vector);
+
+/**
+ * @brief Adds two vectors together
+ *
+ * @param first_vector
+ * @param second_vector
+ *
+ * @return Vector which is the component-wise sum of the given vectors
+ */
+Vector *vector_add(const Vector *first_vector, const Vector *second_vector);
+
+/**
+ * @brief Subtracts one vector from another
+ *
+ * @param first_vector Vector that is subtracted from
+ * @param second_vector Vector that subtracts
+ *
+ * @return Vector which is the component-wise difference of the given vectors
+ */
+Vector *vector_subtract(const Vector *first_vector, const Vector *second_vector);
+
+/**
+ * @brief Computes the dot product of two vectors
+ *
+ * @param first_vector
+ * @param second_vector
+ *
+ * @return Scalar dot product
+ */
+scalar_t vector_dot(const Vector *first_vector, const Vector *second_vector);
+
+/**
+ * @brief Computes the cross product of two 3D vectors
+ *
+ * @param lhs Left hand vector
+ * @param rhs Right hand vector
+ *
+ * @return Vector cross product
+ */
+Vector *vector_cross(const Vector *lhs, const Vector *rhs);
+
+/**
+ * @brief Computes the magnitude of a given vector
+ *
+ * @param *vector Vector whose magnitude will be computed
+ *
+ * @return Magnitude of vector
+ */
+scalar_t mag(const Vector *vector);
+
+/**
+ * @brief Computes the norm of a vector
+ *
+ * @param vector Vector whose norm is computed
+ *
+ * @return Norm of given vector
+ */
+Vector *norm(const Vector *vector);
 
 #endif
